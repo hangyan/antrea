dc43f515dbb407022608995dfa3e49a48d17719e
Hang Yan <yhang@vmware.com>
2024-11-04 19:31:43 +0800

    unit6
    Signed-off-by: Hang Yan <yhang@vmware.com>

--- pkg/agent/packetcapture/packetcapture_controller_linux_test.go
+++ pkg/agent/packetcapture/packetcapture_controller_linux_test.go
@@ -35,7 +35,6 @@ import (
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	"k8s.io/apimachinery/pkg/util/intstr"
-	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/informers"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/kubernetes/fake"
@@ -296,20 +295,20 @@ func TestPacketCaptureControllerRun(t *testing.T) {
 				Packet: &crdv1alpha1.Packet{
 					Protocol: &icmpProto,
 				},
+				FileServer: &crdv1alpha1.PacketCaptureFileServer{},
 			},
 		},
 	}
 
 	pcc := newFakePacketCaptureController(t, nil, []runtime.Object{pc.pc})
+	pcc.sftpUploader = &testUploader{fileName: "pc1.pcapng"}
 	stopCh := make(chan struct{})
 	defer close(stopCh)
 	pcc.crdInformerFactory.Start(stopCh)
 	pcc.crdInformerFactory.WaitForCacheSync(stopCh)
 	pcc.informerFactory.Start(stopCh)
 	pcc.informerFactory.WaitForCacheSync(stopCh)
-	// check the captures that are waiting in line.
-	go pcc.processWaitingCaptures()
-	go wait.Until(pcc.worker, time.Second, stopCh)
+	pcc.startPacketCapture(pc.pc.Name)
 	time.Sleep(300 * time.Millisecond)
 
 	result, nil := pcc.crdClient.CrdV1alpha1().PacketCaptures().Get(context.Background(), pc.pc.Name, metav1.GetOptions{})
